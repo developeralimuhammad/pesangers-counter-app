4f9169eecbf71d529186c0fedf866069418fd58a
Developer-Zunoor <zunooralizunoorali@gmail.com>
2023-06-07 03:38:57 -0700

    added all the files

---
+++ script.js
@@ -0,0 +1,19 @@
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+
+function incriment(){
+    count += 1;
+    countEl.innerText = count;
+}
+
+function save(){
+    countStr = count + "-";
+    saveEl.textContent += countStr;
+
+    countEl.textContent = 0;
+    count = 0;
+
+
+}
